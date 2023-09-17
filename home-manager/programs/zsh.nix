68939c97273d4c2f0a1434f044bb40fd680d4a54
Jeff Windsor <jeff.windsor@gmail.com>
2023-09-17 14:02:39 -0700

    refactor directory envs and aliases

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -11,35 +11,36 @@
   };
 
   sessionVariables = {
-    XDG_STATE_HOME  = "$HOME/.local/state";
-    XDG_DATA_HOME   = "$HOME/.local/share";
-    XDG_CACHE_HOME  = "$HOME/.cache";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    EDITOR          = "$(which nvim)";
-    SRC             = "$HOME/Source";
-    DOTFILES        = "$HOME/.config/";
-    INSTALLS        = "$HOME/.config/installs";
+    XDG_STATE_HOME        = "$HOME/.local/state";
+    XDG_DATA_HOME         = "$HOME/.local/share";
+    XDG_CACHE_HOME        = "$HOME/.cache";
+    XDG_CONFIG_HOME       = "$HOME/.config";
+    EDITOR                = "$(which nvim)";
+    LOC_SRC               = "$HOME/Source";
+    LOC_JEFF              = "$HOME/Source/github.com/jeffwindsor";
+    LOC_NIXOS             = "$HOME/Source/github.com/jeffwindsor/nixos-config";
     GIT_LOG_PRETTY_FORMAT = "%C(green)%h%C(auto)%d%C(reset) - %s | %C(cyan)%an %C(dim)%cr%C(reset)";
     RIPGREP_CONFIG_PATH   = "$XDG_CONFIG_HOME/ripgrep/ripgreprc";
   };
 
   shellAliases = {
     # directories
-    "...."      = "cd ../../../";
-    "..."       = "cd ../../";
-    ".."        = "cd ..";
-    "dot"       = "cd $HOME/.config";
-    "in"        = "cd $HOME/.config/installs/nixos";
-    "src"       = "cd $HOME/Source";
-    "hub"       = "cd $HOME/Source/github.com";
-    "jeff"      = "cd $HOME/Source/github.com/jeffwindsor";
-    "business"  = "cd $HOME/Source/github.com/jeffwindsor/financials/gnucash/business/imports";
-    "home"      = "cd $HOME/Source/github.com/jeffwindsor/financials/gnucash/home/imports";
-    "md"        = "cd $HOME/portable/exocortex/markdown";
+    "...."   = "cd ../../../";
+    "..."    = "cd ../../";
+    ".."     = "cd ..";
+    "dot"    = "cd $XDG_CONFIG_HOME";
+    "config" = "cd $XDG_CONFIG_HOME";
+    "src"    = "cd $LOC_SRC";
+    "hub"    = "cd $LOC_SRC/github.com";
+    "jeff"   = "cd $LOC_JEFF";
+    "in"     = "cd $LOC_NIXOS";
+    "finb"   = "cd $LOC_JEFF/financials/gnucash/business/imports";
+    "finh"   = "cd $LOC_JEFF/financials/gnucash/home/imports";
+    "md"     = "cd $HOME/portable/exocortex/markdown";
     # nvim
-    "v"         = "nvim";
+    "v"      = "nvim";
     # bat / cat
-    "cat"       = "bat --style=plain";
+    "cat"    = "bat --style = plain";
     # exa / ls
     "l"         = "exa -F --group-directories-first";
     "la"        = "exa -F --group-directories-first --git --all";
@@ -71,11 +72,11 @@
     # helix
     "h"         = "helix";
     # nixos
-    "change"    = "nvim -c \":args $XDG_CONFIG_HOME/installs/nixos/**/*.nix\"";
+    "change"    = "nvim -c \":args $LOC_NIXOS/**/*.nix\"";
     "clean"     = "nix-env --delete-generations +7 && nix-collect-garbage";
     "list"      = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
     "optimize"  = "nix store optimise";
-    "rebuild"   = "sudo rm -rf /etc/nixos && sudo cp -fr \$HOME/.config/installs/nixos/ /etc && sudo nixos-rebuild switch";
+    "rebuild"   = "sudo rm -rf /etc/nixos && sudo cp -fr \$LOC_NIXOS/nixos /etc && sudo cp -fr \$LOC_NIXOS/home-manager /etc/nixos && sudo nixos-rebuild switch";
     "shell"     = "nix-shell --command zsh";
     "up"        = "sudo nixos-rebuild switch --upgrade";
   };
