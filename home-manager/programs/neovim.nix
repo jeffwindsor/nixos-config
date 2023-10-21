28dd34f617fe081db62645baf548036a65a7ae16
Jeff Windsor <jeff.windsor@gmail.com>
2023-10-20 19:36:26 -0700

    Revert "hledger aliases improvements"
    This reverts commit 07fc23432f7f788baa832de3e38c85a2ce3313f3.

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -5,7 +5,6 @@
   vimAlias = true;
   vimdiffAlias = true;
 
-  # merge lua neovim config modules
   extraLuaConfig = ''
     ${builtins.readFile(./neovim/config/autocommands.lua)}
     ${builtins.readFile(./neovim/config/keybindings.lua)}
@@ -19,17 +18,8 @@
 
   plugins = with pkgs.vimPlugins; [
     auto-save-nvim
-    cmp-nvim-lsp                    # dependency: cmp
-    cmp_luasnip                     # dependency: cmp
     gitsigns-nvim
-    luasnip                         # dependency: cmp
-    neorg                           # testing this
     nvim-colorizer-lua
-    nvim-treesitter                 # dependency: tree-sitter
-    nvim-treesitter-textobjects     # dependency: tree-sitter
-    plenary-nvim                    # dependency: telescope
-    vim-sleuth                      # auto adjust shift and tab width
-
     {
       plugin = indent-blankline-nvim;
       type   = "lua";
@@ -41,15 +31,15 @@
       config = builtins.readFile(./neovim/plugins/mini-nvim.lua);
     }
     {
-      plugin = nvim-base16;
-      type = "lua";
-      config = builtins.readFile(./neovim/plugins/nvim-base16.lua);
-    }
-    {
       plugin = nvim-cmp;
       type   = "lua";
       config = builtins.readFile(./neovim/plugins/nvim-cmp.lua);
     }
+    cmp-nvim-lsp
+    luasnip
+    cmp_luasnip
+    nvim-treesitter
+    nvim-treesitter-textobjects
     {
       plugin = telescope-nvim;
       type   = "lua";
@@ -60,10 +50,54 @@
       type   = "lua";
       config = builtins.readFile(./neovim/plugins/telescope-file-browser-nvim.lua);
     }
+    plenary-nvim
+    vim-sleuth
     {
       plugin = which-key-nvim;
       type   = "lua";
       config = builtins.readFile(./neovim/plugins/which-key-nvim.lua);
+    }
+    {
+      plugin = nvim-base16;
+      type = "lua";
+
+      # base00 - Default Background
+      # base01 - Lighter Background (Used for status bars, line number and folding marks)
+      # base02 - Selection Background
+      # base03 - Comments, Invisibles, Line Highlighting
+      # base04 - Dark Foreground (Used for status bars)
+      # base05 - Default Foreground, Caret, Delimiters, Operators
+      # base06 - Light Foreground (Not often used)
+      # base07 - Light Background (Not often used)
+      # base08 - Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+      # base09 - Integers, Boolean, Constants, XML Attributes, Markup Link Url
+      # base0A - Classes, Markup Bold, Search Text Background
+      # base0B - Strings, Inherited Class, Markup Code, Diff Inserted
+      # base0C - Support, Regular Expressions, Escape Characters, Markup Quotes
+      # base0D - Functions, Methods, Attribute IDs, Headings
+      # base0E - Keywords, Storage, Selector, Markup Italic, Diff Changed
+      # base0F - Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
+
+      config = with config.colorScheme.colors; ''
+        require('base16-colorscheme').setup({
+            base00 = '#${base00}',
+            base01 = '#${base01}',
+            base02 = '#${base02}',
+            base03 = '#${base03}',
+            base04 = '#${base04}',
+            base05 = '#${base05}',
+            base06 = '#${base06}',
+            base07 = '#${base07}',
+            base08 = '#${base08}',
+            base09 = '#${base09}',
+            base0A = '#${base0A}',
+            base0B = '#${base0B}',
+            base0C = '#${base0C}',
+            base0D = '#${base0D}',
+            base0E = '#${base0E}',
+            base0F = '#${base0F}',
+        })
+      '';
     }
   ];
 
