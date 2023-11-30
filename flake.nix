00e8fd16d863c480f4822cc6e47964f7c3495cac
Jeff Windsor <jeff.windsor@gmail.com>
2023-11-29 18:25:00 -0800

    Refactor

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixpkgs.url      = "github:nixos/nixpkgs/nixos-23.11";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }:
