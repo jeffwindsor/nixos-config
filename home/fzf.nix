469479c02849d3ed878d341fb433eed6d487ffc1
Jeff Windsor <jeff.windsor@gmail.com>
2023-12-08 12:44:41 -0800

    fzf

--- home/fzf.nix
+++ home/fzf.nix
@@ -2,31 +2,31 @@
   let
     height = "--height=60%";
     layout = "--layout=reverse";
-    info = "--info=inline";
+    info = "--info=hidden";
     enter_to_edit = "--bind='enter:become(hx {})'";
-    find_as_dir   = "fd --hidden --type d";
-    find_as_file    = "fd --hidden --type f";
+    find = "fd --hidden";
+    only_dirs = " --type d";
+    only_files = " --type f";
     preview_as_file = "--preview='bat --style=numbers --color=always {}'";
-    preview_as_dir  = "--preview 'eza --tree --git {}'";
+    preview_as_dir = "--preview 'eza --tree --git {}'";
     preview_pager = "--bind 'ctrl-/:change-preview-window(down|hidden|)'";
   in 
 {
   programs.fzf = {
     enable = true;
     enableBashIntegration = true; 
-    enableZshIntegration = true; 
 
     # fzf
-    defaultCommand = find_as_file;
-    defaultOptions = [height layout info enter_to_edit preview_as_file preview_pager];
+    defaultCommand = find;
+    defaultOptions = [height layout info];
   
     # Alt - C
-    changeDirWidgetCommand = find_as_dir;
+    changeDirWidgetCommand = (find + only_dirs);
     changeDirWidgetOptions = [height layout info preview_as_dir preview_pager];
 
     # Control - T
-    fileWidgetCommand = find_as_file;
-    fileWidgetOptions = [height layout info preview_as_file preview_pager];
+    fileWidgetCommand = (find + only_files);
+    fileWidgetOptions = [height layout info preview_as_file preview_pager enter_to_edit];
 
     # Control - R
     # historyWidgetCommand = "";
