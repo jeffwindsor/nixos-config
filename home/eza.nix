81e8dd6c9295a3b156ea96a3de28ee33a06da8d5
Jeff Windsor <jeff.windsor@gmail.com>
2023-12-23 23:03:49 -0800

    eza tree options match list options

--- home/eza.nix
+++ home/eza.nix
@@ -4,8 +4,8 @@
 	home.shellAliases = {
     "ll"       = "eza -lF --group-directories-first";
     "lla"      = "eza -lF --group-directories-first --git --all";
-    "tree"     = "eza --tree --git";
-    "treea"    = "eza -lF --tree --git --all";
+    "tree"     = "eza --tree -lF --group-directories-first";
+    "treea"    = "eza --tree -lF --group-directories-first --git --all";
   };
 	
 	home.packages = with pkgs; [ eza ];
